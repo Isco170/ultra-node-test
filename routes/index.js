e64361dd30b105ec5b1b563531216198af46261b
Isco170 <francisco_andre_mavie>
2021-11-11 19:18:03 +0200

    Links

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({response: 'Ultra Node Test'});
+  res.send({
+    producao: 'https://ultra-node-test.herokuapp.com/',
+    Zip: 'https://drive.google.com/file/d/1FYhF82xg151Iw4TSUPph4bIA2Ejx156r/view?usp=sharing'
+  });
 });
 
 module.exports = router;
